535a3c6e79d098c2486edcc456efc8bf4bebcaab
YaAnuar <42729761+YaAnuar@users.noreply.github.com>
2022-08-05 13:28:13 +0600

    Update employee.py

--- app/routers/employee.py
+++ app/routers/employee.py
@@ -1,6 +1,6 @@
 from app.routers import AsyncSession, get_session, select, Depends, selectinload, Request, APIRouter
 from app.routers import HTTPException
-from app.models.employee import EmployeeBase, Employee, EmployeeCreate, EmployeeUpdate, EmployeeReadAll
+from app.models.employee import EmployeeBase, Employee, EmployeeCreate, EmployeeUpdate, EmployeeReadAll, Employee_validate
 from pydantic import ValidationError
 
 
@@ -29,10 +29,9 @@ async def get_employee_by_id(empl_id: int, session: AsyncSession = Depends(get_s
 async def add_employee(request: Request, session: AsyncSession = Depends(get_session)):
     req = await request.json()
     try:
-        EmployeeBase.validate({"id_person": req['id_person'], "id_department": req['id_department'], 
-                                        "tab": req['tab'], "hire_date": req['hire_date'], "dismissal_date": req['dismissal_date']})
+        Employee_validate(req)
     except ValidationError as e:
-        return  HTTPException(status_code=404, detail="Incorrect value: " + str(e))
+        return  HTTPException(status_code=400, detail="Incorrect value: " + str(e))
     else:
         empl = Employee(tab=req['tab'], 
                         id_person=req['id_person'], 
