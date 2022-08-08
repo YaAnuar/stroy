b7131bd1b2d1dd3f10badc580f394c9be129ca0f
YaAnuar <42729761+YaAnuar@users.noreply.github.com>
2022-08-08 19:53:22 +0600

    fix database url

--- app/config/db.py
+++ app/config/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/orders"
+DATABASE_URL = os.environ["DATABASE_URL"]
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
 
